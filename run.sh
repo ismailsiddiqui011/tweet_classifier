6eacb36d8b6fca23203ef42e4ca9a1c37ac46a00
ismailsiddiqui011 <mohdismailsiddiqui011@gmail.com>
2024-10-12 21:40:48 +0530

    weight download

--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
 set -m
+wget https://media.githubusercontent.com/media/ismailsiddiqui011/tweet_classifier/refs/heads/main/fine_tuned_t5/model.safetensors fine_tuned_t5/model.safetensors
 
 python main.py &
 
