fb4f920882c322197a725e2a83d7f4d9b7dc1057
ismailsiddiqui011 <mohdismailsiddiqui011@gmail.com>
2024-10-12 21:52:53 +0530

    fix

--- run.sh
+++ run.sh
@@ -1,5 +1,6 @@
 set -m
-wget https://media.githubusercontent.com/media/ismailsiddiqui011/tweet_classifier/refs/heads/main/fine_tuned_t5/model.safetensors fine_tuned_t5/model.safetensors
+rm /tweet_classifier/fine_tuned_t5/model.safetensors
+wget https://media.githubusercontent.com/media/ismailsiddiqui011/tweet_classifier/refs/heads/main/fine_tuned_t5/model.safetensors /tweet_classifier/fine_tuned_t5/model.safetensors
 
 python main.py &
 
